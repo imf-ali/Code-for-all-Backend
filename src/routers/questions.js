13755531cf372c70de35561a3747325c9af9941d
JazibEqbal <jazibeqbal0526@gmail.com>
2022-10-30 18:52:40 +0530

    StatusCodes

--- src/routers/questions.js
+++ src/routers/questions.js
@@ -1,75 +1,79 @@
-const express = require('express');
-const auth = require('../middleware/auth');
-const Questions = require('../models/questions');
-const Contests = require('../models/contests');
+const express = require("express");
+const auth = require("../middleware/auth");
+const Questions = require("../models/questions");
+const Contests = require("../models/contests");
+import StatusCodes from "http-status-codes";
 
-const Router = new express.Router()
+const Router = new express.Router();
 
-Router.post('/questions', auth, async (req,res) => {
-    const ques = new Questions(req.body);
-    const contestId  = req.body.contest;
-    try{
-        const savedQues = await ques.save();
-        await Contests.findByIdAndUpdate(contestId,{ 
-            $push: { question: savedQues._id } 
-        },
-        { 
-            new: true, useFindAndModify: false 
-        },
-        );
-        res.status(201).send(savedQues)
-    }catch(e){  
-        console.log(e);
-        res.status(400).send(e)
+Router.post("/questions", auth, async (req, res) => {
+  const ques = new Questions(req.body);
+  const contestId = req.body.contest;
+  try {
+    const savedQues = await ques.save();
+    await Contests.findByIdAndUpdate(
+      contestId,
+      {
+        $push: { question: savedQues._id },
+      },
+      {
+        new: true,
+        useFindAndModify: false,
+      }
+    );
+    res.status(StatusCodes.OK).send(savedQues);
+  } catch (e) {
+    console.log(e);
+    res.status(StatusCodes.BAD_REQUEST).send(e);
+  }
+});
+
+Router.put("/questions/:questionId", auth, async (req, res) => {
+  try {
+    const ques = await Questions.findById(req.params.questionId);
+    ques.question = req.body.question;
+    ques.save();
+    res.status(StatusCodes.OK).send(ques);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e);
+  }
+});
+
+Router.put("/questions/solution/:questionId", auth, async (req, res) => {
+  const answer = req.body.answer;
+  const questionId = req.params.questionId;
+  try {
+    const savedQues = await Questions.findById(questionId);
+    if (answer) {
+      savedQues.answer = answer;
+      savedQues.solution = true;
     }
-})
+    const ans = await savedQues.save();
+    res.status(StatusCodes.OK).send(ans);
+  } catch (e) {
+    console.log(e);
+    res.status(StatusCodes.BAD_REQUEST).send(e);
+  }
+});
 
-Router.put('/questions/:questionId', auth, async (req,res) => {
-    try {
-        const ques = await Questions.findById(req.params.questionId);
-        ques.question = req.body.question;
-        ques.save();
-        res.status(201).send(ques);
-    } catch (e) {
-        console.log(e);
-        res.status(400).send(e)
-    }
-})
+Router.get("/questions/:questionId", async (req, res) => {
+  try {
+    const question = await Questions.findById(req.params.questionId);
+    res.status(StatusCodes.OK).send(question);
+  } catch (e) {
+    res.status(StatusCodes.BAD_REQUEST).send(e);
+  }
+});
 
-Router.put('/questions/solution/:questionId', auth, async (req,res) => {
-    const answer = req.body.answer;
-    const questionId = req.params.questionId;
-    try {
-        const savedQues = await Questions.findById(questionId);
-        if(answer){
-            savedQues.answer = answer;
-            savedQues.solution = true;
-        }
-        const ans = await savedQues.save();
-        res.status(201).send(ans)
-    } catch (e) {
-        console.log(e)
-        res.status(400).send(e)
-    }
-})
+Router.get("/questions/contest/:contestId", async (req, res) => {
+  try {
+    const contestData = await Questions.find({ contest: req.params.contestId });
+    res.status(StatusCodes.OK).send(contestData);
+  } catch (e) {
+    console.log(e);
+    res.status(StatusCodes.BAD_REQUEST).send(e);
+  }
+});
 
-Router.get('/questions/:questionId', async (req,res) => {
-    try{
-        const question = await Questions.findById(req.params.questionId);
-        res.status(200).send(question);
-    }catch(e){
-        res.status(401).send(e)
-    }
-})
-
-Router.get('/questions/contest/:contestId', async (req,res) => {
-    try{
-        const contestData = await Questions.find({ contest: req.params.contestId})
-        res.status(200).send(contestData)
-    }catch(e){
-        console.log(e);
-        res.status(400).send(e)
-    }
-})
-
-module.exports = Router
+module.exports = Router;
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,39 +1,45 @@
-const express = require('express')
-const User = require('../models/user')
-const Router = new express.Router()
-const auth = require('../middleware/auth')
+const express = require("express");
+const User = require("../models/user");
+const Router = new express.Router();
+const auth = require("../middleware/auth");
+import StatusCodes from "http-status-codes";
 
-Router.post('/user', async (req, res) => {
-    const user = new User(req.body)
+Router.post("/user", async (req, res) => {
+  const user = new User(req.body);
 
-    try{
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.status(201).send({user , token})
-    }catch(e){
-        res.status(400).send()
-    }
-})
+  try {
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(StatusCodes.CREATED).send({ user, token });
+  } catch (e) {
+    res.status(StatusCodes.BAD_REQUEST).send();
+  }
+});
 
-Router.post('/user/login', async (req,res) => {
-    try{
-        const user = await User.findByCredentials(req.body.email,req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({user , token})    
-    }catch(e){
-        res.status(401).send()
-    }
-})
+Router.post("/user/login", async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.status(StatusCodes.CREATED).send({ user, token });
+  } catch (e) {
+    res.status(StatusCodes.BAD_REQUEST).send();
+  }
+});
 
-Router.post('/user/logout', auth , async (req,res) => {
-    try {
-        req.user.tokens = req.user.tokens.filter((token)=> token.token !== req.token)
-        await req.user.save()
+Router.post("/user/logout", auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(
+      (token) => token.token !== req.token
+    );
+    await req.user.save();
 
-        res.send()
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+    res.send();
+  } catch (e) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+  }
+});
 
-module.exports = Router
+module.exports = Router;
